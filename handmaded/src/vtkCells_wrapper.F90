d69f8cff229548ccce4e8a2ea4a3dfc7d5a35000
sato-hajime <h-sato@advancesoft.jp>
2018-07-23 15:33:01 +0900

    vtkCells_wrapperを更新

--- handmaded/src/vtkCells_wrapper.F90
+++ handmaded/src/vtkCells_wrapper.F90
@@ -3,6 +3,7 @@ module m_vtkCells_wrapper
   
   use m_vtkCell
   use m_vtkDataSet
+  use m_vtkObjectBase
 
 #if defined(FORTRAN2003_CLASS)
   use m_vtkDataSetClass
@@ -10,11 +11,13 @@ module m_vtkCells_wrapper
   
   implicit none
 
+  interface vtkCells_wrap
+     module procedure vtkCells_wrap_vtkDataSet
+     ! module procedure vtkCells_wrap_cellTypes_cellArray
 #if defined(FORTRAN2003_CLASS)
-  interface vtkCells_wrapper
-     module procedure vtkCells_wrapper_vtkDataSetClass
-  end interface vtkCells_wrapper
+     module procedure vtkCells_wrap_vtkDataSetClass
 #endif
+  end interface vtkCells_wrap
   
   type :: vtkCell_wrapper
      integer(c_int)             :: type = -1
@@ -38,25 +41,27 @@ contains
     
   end subroutine vtkCells_wrapper_vtkCell
 
-  subroutine vtkCells_wrapper_vtkDataSet(dataSet, cell_wrappers)
+  subroutine vtkCells_wrap_vtkDataSet(dataSet, cell_wrappers)
     implicit none
     type(c_ptr),intent(in)            :: dataSet
     type(vtkCell_wrapper),allocatable :: cell_wrappers(:)
 
+    type(c_ptr) :: cell
+    
     integer :: iCell
     allocate( cell_wrappers( 0:vtkDataSet_GetNumberOfCells(dataSet)-1 ))
     do iCell = lbound(cell_wrappers,1), ubound(cell_wrappers,1)
-       call vtkCells_wrapper_vtkCell(&
-            vtkDataSet_GetCell(dataSet, iCell), cell_wrappers(iCell))
+       cell = vtkDataSet_GetCell(dataSet, iCell)
+       call vtkCells_wrapper_vtkCell(cell, cell_wrappers(iCell))
     end do
-  end subroutine vtkCells_wrapper_vtkDataSet
+  end subroutine vtkCells_wrap_vtkDataSet
 
 #if defined(FORTRAN2003_CLASS)
-  subroutine vtkCells_wrapper_vtkDataSetClass(dataset, cell_wrappers)
+  subroutine vtkCells_wrap_vtkDataSetClass(dataset, cell_wrappers)
     class(vtkDataSet)                 :: dataSet
     type(vtkCell_wrapper),allocatable :: cell_wrappers(:)
-    call vtkCells_wrapper_vtkDataSet(dataSet%ptr, cell_wrappers)
-  end subroutine vtkCells_wrapper_vtkDataSetClass
+    call vtkCells_wrap_vtkDataSet(dataSet%ptr, cell_wrappers)
+  end subroutine vtkCells_wrap_vtkDataSetClass
 #endif
   
 end module m_vtkCells_wrapper
